00cb13f6b5ddbacae6081c745508f9acfb9bd980
rhit-liub5 <95311554+rhit-liub5@users.noreply.github.com>
2026-09-02 15:30:12 +0800

    first practis

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -3,6 +3,8 @@ package com.example.restservice;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +17,13 @@ public class GreetingController {
   @GetMapping("/greeting")
   public Greeting greeting(@RequestParam(defaultValue = "World") String name) {
     return new Greeting(counter.incrementAndGet(), template.formatted(name));
+  }
+
+  @PostMapping("/greeting")
+  public Greeting greetingPost(@RequestBody GreetingRequest request){
+          return new Greeting(
+            counter.incrementAndGet(),
+            template.formatted(request.name())
+        );
   }
 }
